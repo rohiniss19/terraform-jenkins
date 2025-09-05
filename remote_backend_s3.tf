ff28440b88fe20542496cc55640e3e555e775121
Rohini <rohinissankannavar@gmail.com>
2025-09-06 00:05:44 +0530

    t

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-state-bucket-123456"
+    bucket = "dev-proj-1-jenkins-remote-state-bucket-roh"
     key    = "devops-project-1/jenkins/terraform.tfstate"
-    region = "eu-west-1a"
+    region = "eu-west-1"
   }
 }
