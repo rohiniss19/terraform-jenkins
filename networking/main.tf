80cdc8b1d93e25c679047394428ea29b7fe3a7f1
Rohini <rohinissankannavar@gmail.com>
2025-09-06 01:28:42 +0530

    t

--- networking/main.tf
+++ networking/main.tf
@@ -58,7 +58,7 @@ resource "aws_internet_gateway" "dev_proj_1_public_internet_gateway" {
 }
 
 # Public Route Table
-/*resource "aws_route_table" "dev_proj_1_public_route_table" {
+resource "aws_route_table" "dev_proj_1_public_route_table" {
   vpc_id = aws_vpc.dev_proj_1_vpc_eu_central_1.id
   route {
     cidr_block = "0.0.0.0/0"
@@ -90,4 +90,4 @@ resource "aws_route_table_association" "dev_proj_1_private_rt_subnet_association
   count          = length(aws_subnet.dev_proj_1_private_subnets)
   subnet_id      = aws_subnet.dev_proj_1_private_subnets[count.index].id
   route_table_id = aws_route_table.dev_proj_1_private_subnets.id
-}*/
+}
